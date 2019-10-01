sunnyku/ceph-ci

osd/PeeringState: track down OSDs relevant to prior_readable_until_ub
Keep track of which OSDs from the prior set we care about that affect
the prior_readable_until_ub.  Note that it is only the *down* OSDs that
we have to track here, since everything in the *probe* set we will already
contact during peering (they are still up), guaranteeing that those PGs
are aware of the interval change and are no longer readable in the prior
interval.

Signed-off-by: Sage Weil <sage@redhat.com>

src/osd/PeeringState.h
@@ -1365,8 +1365,12 @@ public:
 
   /// upper bound on any acting OSDs' readable_until in this interval
   ceph::signedspan readable_until_ub = ceph::signedspan::zero();
+
   /// upper bound from prior interval(s)
   ceph::signedspan prior_readable_until_ub = ceph::signedspan::zero();
+
+  /// pg instances from prior interval(s) that may still be readable
+  set<int> prior_readable_down_osds;
 
   /// [replica] upper bound we got from the primary (primary's clock)
   ceph::signedspan readable_until_ub_from_primary = ceph::signedspan::zero();
@@ -1961,9 +1965,15 @@ public:
     return prior_readable_until_ub;
   }
 
+  /// Get prior intervals' readable_until down OSDs of note
+  const set<int>& get_prior_readable_down_osds() const {
+    return prior_readable_down_osds;
+  }
+
   /// Reset prior intervals' readable_until upper bound (e.g., bc it passed)
   void clear_prior_readable_until_ub() {
     prior_readable_until_ub = ceph::signedspan::zero();
+    prior_readable_down_osds.empty();
   }
 
   void renew_lease(ceph::signedspan now) {

aminator006/ceph

src/osd/PeeringState.h
@@ -1364,8 +1364,12 @@ public:
 
   /// upper bound on any acting OSDs' readable_until in this interval
   ceph::signedspan readable_until_ub = ceph::signedspan::zero();
+
   /// upper bound from prior interval(s)
   ceph::signedspan prior_readable_until_ub = ceph::signedspan::zero();
+
+  /// pg instances from prior interval(s) that may still be readable
+  set<int> prior_readable_down_osds;
 
   /// [replica] upper bound we got from the primary (primary's clock)
   ceph::signedspan readable_until_ub_from_primary = ceph::signedspan::zero();
@@ -1959,9 +1963,15 @@ public:
     return prior_readable_until_ub;
   }
 
+  /// Get prior intervals' readable_until down OSDs of note
+  const set<int>& get_prior_readable_down_osds() const {
+    return prior_readable_down_osds;
+  }
+
   /// Reset prior intervals' readable_until upper bound (e.g., bc it passed)
   void clear_prior_readable_until_ub() {
     prior_readable_until_ub = ceph::signedspan::zero();
+    prior_readable_down_osds.clear();
   }
 
   void renew_lease(ceph::signedspan now) {